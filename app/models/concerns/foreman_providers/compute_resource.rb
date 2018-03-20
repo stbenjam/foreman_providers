cf584b80e2ac6dca7ad2a0f2657f6a83ace03131
Adam Grare <agrare@redhat.com>
2018-03-20 11:27:49 -0400

    Create and destroy provider with compute_resources

---
+++ app/models/concerns/foreman_providers/compute_resource.rb
@@ -0,0 +1,30 @@
+module ForemanProviders
+  module ComputeResource
+    extend ActiveSupport::Concern
+
+    included do
+      after_create :create_provider
+      before_destroy :destroy_provider
+    end
+
+    def create_provider
+      provider_klass = foreman_type_to_provider_type.constantize
+
+      ems = provider_klass.create!(:name => name, :hostname => URI(url).host)
+      ems.update_authentication(:default => {:userid => user, :password => password})
+      ems.default_endpoint.update_attributes(:verify_ssl => 0)
+    end
+
+    def destroy_provider
+      provider_klass = foreman_type_to_provider_type.constantize
+      provider_klass.find_by(:hostname => URI(url).host).try(:destroy)
+    end
+
+    def foreman_type_to_provider_type
+      case type
+      when "Foreman::Model::Ovirt"
+        "Providers::Ovirt::Manager"
+      end
+    end
+  end
+end
